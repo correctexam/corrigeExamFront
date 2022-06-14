bb844691eb7577477a17e6995488a2ce3962a314
arnobl <1052622+arnobl@users.noreply.github.com>
2022-06-14 10:38:19 +0200

    feat(marking-summary): code improvements of the marking summary pages

--- src/main/webapp/app/scanexam/marking-summary/marking-summary.component.ts
+++ src/main/webapp/app/scanexam/marking-summary/marking-summary.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { QuestionService } from '../../entities/question/service/question.service';
 import { StudentService } from '../../entities/student/service/student.service';
 import { StudentResponseService } from '../../entities/student-response/service/student-response.service';
+import { IExam } from '../../entities/exam/exam.model';
 
 @Component({
   selector: 'jhi-marking-summary',
@@ -31,33 +32,7 @@ export class MarkingSummaryComponent implements OnInit {
         const exam = dataExam.body;
 
         if (exam) {
-          this.nameExam = exam.name ?? '';
-
-          this.studentService.query({ courseId: exam.courseId }).subscribe(dataStd => {
-            this.nbStd = dataStd.body?.length ?? 0;
-          });
-
-          this.studentResponseService.query().subscribe(dataMarking => {
-            const marks = dataMarking.body ?? [];
-
-            this.questionService.query({ examId: exam.id }).subscribe(dataQuestion => {
-              const questions = dataQuestion.body ?? [];
-              const questionsSerie = Array.from(Array(questions.length - 1).keys()).map(x => x + 1);
-
-              questions.forEach(q => {
-                const marksQ = marks.filter(m => m.questionId === q.id);
-                const countMarked = marksQ.length;
-                const markedSheets = marksQ.filter(m => m.sheetId).map(m => m.sheetId!);
-                const remainingSheets = questionsSerie.filter(sheet => !markedSheets.includes(sheet));
-
-                this.questions.push({
-                  answeredSheets: countMarked,
-                  number: q.numero ?? -1,
-                  firstSheetNotAnswered: remainingSheets.length === 0 ? 1 : remainingSheets[0],
-                });
-              });
-            });
-          });
+          this.fillQuestionsTableSummary(exam);
         }
       });
     });
@@ -74,8 +49,51 @@ export class MarkingSummaryComponent implements OnInit {
 
     return this.questions.map(q => q.answeredSheets).reduce((q1, q2) => q1 + q2);
   }
+
+  /**
+   * Fills the table that summarizes the questions markings using the given exam.
+   * @param exam The exam to use for filling the table.
+   */
+  private fillQuestionsTableSummary(exam: IExam): void {
+    this.nameExam = exam.name ?? '';
+
+    // Getting the students involved in the current course
+    this.studentService.query({ courseId: exam.courseId }).subscribe(dataStd => {
+      this.nbStd = dataStd.body?.length ?? 0;
+    });
+
+    // Getting all the marks of the current exam
+    // TODO: does the query only return the remarks of the ongoing exam? Not able to use parameters
+    this.studentResponseService.query().subscribe(dataMarking => {
+      const marks = dataMarking.body ?? [];
+
+      // Getting all the questions of the current exam
+      this.questionService.query({ examId: exam.id }).subscribe(dataQuestion => {
+        const questions = dataQuestion.body ?? [];
+        // Used to identify the first sheet not marked
+        const questionsSerie = Array.from(Array(questions.length - 1).keys()).map(x => x + 1);
+
+        questions.forEach(q => {
+          const marksQ = marks.filter(m => m.questionId === q.id);
+          const countMarked = marksQ.length;
+          const markedSheets = marksQ.filter(m => m.sheetId).map(m => m.sheetId!);
+          // removing the marked sheets from 'questionsSerie' to get the unmarked questions
+          const remainingSheets = questionsSerie.filter(sheet => !markedSheets.includes(sheet));
+
+          this.questions.push({
+            answeredSheets: countMarked,
+            number: q.numero ?? -1,
+            firstSheetNotAnswered: remainingSheets.length === 0 ? 1 : remainingSheets[0],
+          });
+        });
+      });
+    });
+  }
 }
 
+/**
+ * Local type used in the summary table
+ */
 interface QuestionSummary {
   number: number;
   answeredSheets: number;
