2e28704bd37ccd6e1be55ccd037f471e814f5e00
arnobl <1052622+arnobl@users.noreply.github.com>
2023-01-12 12:30:22 +0100

    fix(stat): incorrect values for questions

--- src/main/webapp/app/scanexam/statsexam/statsexam.component.ts
+++ src/main/webapp/app/scanexam/statsexam/statsexam.component.ts
@@ -1,11 +1,6 @@
-/* eslint-disable @typescript-eslint/no-inferrable-types */
-/* eslint-disable @typescript-eslint/member-ordering */
-/* eslint-disable no-empty */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable guard-for-in */
 /* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/member-ordering */
+/* eslint-disable guard-for-in */
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -18,13 +13,9 @@ import { Observable } from 'rxjs';
 import { db } from '../db/db';
 
 // Couleurs à utiliser
-const GRIS_LEGER = 'rgba(179,181,198,0.2)';
 const GRIS = 'rgba(179,181,198,1)';
-const VERT_LEGER = 'rgba(120,255,132,0.2)';
 const VERT = 'rgba(120,255,132,1)';
-const ROUGE_LEGER = 'rgb(255, 120, 120,0.2)';
 const ROUGE = 'rgb(255, 120, 120)';
-const BLEU_FONCE_LEGER = 'rgb(72, 61, 139,0.2)';
 const BLEU_FONCE = 'rgb(72, 61, 139)';
 const BLANC = 'rgba(255,255,255,1)';
 const VIOLET = 'rgb(233, 120, 255)';
@@ -32,7 +23,6 @@ const VIOLET_TIEDE = 'rgb(233, 120, 255,0.6)';
 const VIOLET_LEGER = 'rgb(233, 120, 255,0.2)';
 const BLEU_AERO = 'rgb(142, 184, 229)';
 const BLEU_AERO_TIEDE = 'rgb(142, 184, 229,0.6)';
-const BLEU_AERO_LEGER = 'rgb(142, 184, 229,0.2)';
 const TRANSPARENT = 'rgba(255,255,255,0.0)';
 
 @Component({
@@ -42,15 +32,19 @@ const TRANSPARENT = 'rgba(255,255,255,0.0)';
 })
 export class StatsExamComponent implements OnInit {
   // Page related variables
-  examid: string | undefined = undefined;
+  examid = '-1';
   infosQuestions: IQuestion[] = [];
   infosStudents: StudentRes[] = [];
   q_notees: QuestionNotee[] = [];
   notes_eleves: number[] = [];
-  choixTri: boolean = true;
+  choixTri = true;
 
-  // Variables d'affichage
-  data_radar_courant!: IRadar;
+  // Graphical data
+  data_radar_courant: IRadar = {
+    labels: [],
+    datasets: [],
+    vue: '',
+  };
   etudiantSelec: StudentRes | null | undefined;
   listeMobileEtudiant: StudSelecMobile[] = [];
   mobileSortChoices: ISortMobile[] = [
@@ -61,9 +55,9 @@ export class StatsExamComponent implements OnInit {
   mobileSortChoice: ISortMobile = this.mobileSortChoices[2];
   knobsCourants: string[] = [];
   COLOR_KNOBS = BLEU_AERO_TIEDE;
-  idQuestionSelected: number = 0;
-  questionSelectionnee: boolean = false;
-  texte_correction: string = 'Correction';
+  idQuestionSelected = 0;
+  questionSelectionnee = false;
+  texte_correction = 'Correction';
   readonly ICONSORTUP = 'pi pi-sort-amount-up-alt'; // Permet d'éviter une étrange erreur de vscode (Unexpected keyword or identifier.javascript)
 
   activeIndex = 1;
@@ -102,14 +96,14 @@ export class StatsExamComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe(params => {
-      if (params.get('examid') !== null) {
-        this.examid = params.get('examid')!;
+      this.examid = params.get('examid') ?? '-1';
 
-        if (this.examid !== params.get('examid')! || this.images.length === 0) {
-          this.examid = params.get('examid')!;
+      if (this.examid !== '-1') {
+        if (this.images.length === 0) {
           this.loadAllPages();
         }
-        this.translateService.get('scanexam.noteattribuee').subscribe(data => {
+
+        this.translateService.get('scanexam.noteattribuee').subscribe(() => {
           this.initStudents().then(() =>
             this.requeteInfoQuestions().subscribe(b => {
               this.infosQuestions = b.body;
@@ -135,7 +129,7 @@ export class StatsExamComponent implements OnInit {
   loadAllPages(): void {
     db.templates
       .where('examId')
-      .equals(+this.examid!)
+      .equals(+this.examid)
       .count()
       .then(e2 => {
         this.nbreFeuilleParCopie = e2;
@@ -144,7 +138,7 @@ export class StatsExamComponent implements OnInit {
 
     if (this.noalign) {
       db.nonAlignImages
-        .where({ examId: +this.examid! })
+        .where({ examId: +this.examid })
         .sortBy('pageNumber')
         .then(e1 =>
           e1.forEach(e => {
@@ -159,7 +153,7 @@ export class StatsExamComponent implements OnInit {
         );
     } else {
       db.alignImages
-        .where({ examId: +this.examid! })
+        .where({ examId: +this.examid })
         .sortBy('pageNumber')
         .then(e1 =>
           e1.forEach(e => {
@@ -206,8 +200,7 @@ export class StatsExamComponent implements OnInit {
 
   private async loadStudents(): Promise<StudentRes[]> {
     return new Promise<StudentRes[]>(res => {
-      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-      this.http.get<StudentRes[]>(this.applicationConfigService.getEndpointFor('api/showResult/' + this.examid)).subscribe(s => {
+      this.http.get<StudentRes[]>(this.applicationConfigService.getEndpointFor(`api/showResult/${this.examid}`)).subscribe(s => {
         res(s);
       });
     });
@@ -217,8 +210,8 @@ export class StatsExamComponent implements OnInit {
     this.triNotes(this.infosStudents);
     const qn: QuestionNotee[] = [];
     for (const q of this.infosQuestions) {
-      const bareme: number = q.point !== undefined ? q.point : 0;
-      const label: string = ' Question ' + (q.numero !== undefined ? q.numero : '0').toString();
+      const bareme = q.point ?? 0;
+      const label = ` Question ${q.numero ?? '0'}`;
       const notesAssociees: number[] = [];
       const quest_divisee = qn.find(quest => quest.label === label);
       if (quest_divisee === undefined) {
@@ -227,20 +220,17 @@ export class StatsExamComponent implements OnInit {
         qn.push({ bareme, label, notesAssociees });
       }
     }
-    for (const s of this.infosStudents) {
-      if (s.ine !== '') {
-        let indiceNote = 0; // On suppose que l'ordre entre infosQuestions et notequestions est le même
+    this.infosStudents
+      .filter(s => s.ine !== '')
+      .forEach(s => {
         for (const key in s.notequestions) {
-          // const indiceNote = parseInt(key, 10);
-          const note = this.s2f(s.notequestions[key]); // La note associée est décrite avec une virgule et non un point comme séparateur décimal
-          qn[indiceNote].notesAssociees.push(note);
-          indiceNote++;
+          // semi-column char instead of decimal dot, so:
+          const note = this.s2f(s.notequestions[key]);
+          qn[parseFloat(key) - 1]?.notesAssociees?.push(note);
         }
-        // Stockage de la note de l'élève
         const note = s.note === undefined ? 0 : this.s2f(s.note);
         this.notes_eleves.push(note);
-      }
-    }
+      });
     this.q_notees = qn;
   }
 
@@ -351,8 +341,12 @@ export class StatsExamComponent implements OnInit {
   /** @return la moyenne associée à chaque question */
   private getMoyennesQuestions(): number[] {
     const moyennes: number[] = [];
+    console.log('in getMoyennesQuestions');
+    console.log(this.q_notees);
+    console.log('avg:');
     for (const qn of this.q_notees) {
       moyennes.push(this.avg(qn.notesAssociees));
+      console.log(this.avg(qn.notesAssociees));
     }
     return moyennes;
   }
@@ -360,9 +354,13 @@ export class StatsExamComponent implements OnInit {
   private updateKnobs(): void {
     const knobsNb = this.etudiantSelec !== null && this.etudiantSelec !== undefined ? this.getNotesSelect() : this.getMoyennesQuestions();
     this.knobsCourants = [];
+    console.log('==>');
+    console.log(this.etudiantSelec);
+    console.log(knobsNb);
     knobsNb.forEach(knobValue => {
       this.knobsCourants.push(knobValue.toFixed(2));
     });
+    console.log(this.knobsCourants);
   }
 
   public getMoyenneExam(): number {
@@ -459,7 +457,7 @@ export class StatsExamComponent implements OnInit {
     return Math.sqrt(this.var(tab));
   }
 
-  private normaliseNotes(notes: number[], baremes: number[], norme: number = 100): number[] {
+  private normaliseNotes(notes: number[], baremes: number[], norme = 100): number[] {
     //  expect(notes.length).toBe(baremes.length);
     notes.forEach((note, indice) => {
       note /= baremes[indice];
@@ -492,15 +490,15 @@ export class StatsExamComponent implements OnInit {
     }
   }
 
-  private s_red(s: string, length: number = 3, abrevSymbol: string = '.'): string {
+  private s_red(s: string, length = 3, abrevSymbol = '.'): string {
     if (length >= s.length) {
       return s;
     }
     return s.slice(0, length) + abrevSymbol;
   }
 
-  /** @initialise les données à afficher dans le radar de visualisation globale*/
-  private initGlobalRadarData(stats: QuestionNotee[], pourcents: boolean = false): IRadar {
+  /** Initialises radar data to display **/
+  private initGlobalRadarData(stats: QuestionNotee[], pourcents = false): IRadar {
     const labels: string[] = [];
     for (const stat of stats) {
       labels.push(stat.label);
@@ -516,7 +514,7 @@ export class StatsExamComponent implements OnInit {
     return { labels, datasets, vue };
   }
 
-  private initStudentRadarData(etudiant: StudentRes, pourcents: boolean = false): IRadar {
+  private initStudentRadarData(etudiant: StudentRes, pourcents = false): IRadar {
     const labels = this.data_radar_courant.labels;
     const datasets = [this.radarStudent(etudiant), this.radarMoy(), this.radarMed()];
     if (pourcents) {
@@ -580,7 +578,7 @@ export class StatsExamComponent implements OnInit {
     this.updateCarte('questions_stats', undefined, selection, infosExam);
   }
 
-  onStudentSelect(event: any): void {
+  onStudentSelect(): void {
     if (this.etudiantSelec !== null && this.etudiantSelec !== undefined) {
       //  this.updateCarte('selection_etudiant', undefined, this.etudiantSelec.prenom + ' ' + this.etudiantSelec.nom, undefined);
       this.data_radar_courant = this.initStudentRadarData(
@@ -592,7 +590,7 @@ export class StatsExamComponent implements OnInit {
       this.COLOR_KNOBS = VIOLET_TIEDE;
     }
   }
-  onStudentUnselect(event: any): void {
+  onStudentUnselect(): void {
     // this.updateCarte('selection_etudiant', undefined, this.translateService.instant('scanexam.nostudentselected'), undefined);
     this.updateCarteRadar();
     this.updateKnobs();
@@ -694,8 +692,7 @@ export class StatsExamComponent implements OnInit {
   }
 
   public goToCorrection(): void {
-    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    location.href = 'answer/' + this.examid + '/' + (this.idQuestionSelected + 1) + '/' + this.etudiantSelec?.studentNumber?.toString();
+    location.href = `answer/${this.examid}/${this.idQuestionSelected + 1}/${this.etudiantSelec?.studentNumber?.toString() ?? ''}`;
   }
   public voirLaCopie(): void {
     if (this.etudiantSelec?.studentNumber?.toString() !== undefined) {
@@ -706,8 +703,7 @@ export class StatsExamComponent implements OnInit {
     this.displayBasic = true;
   }
   gotoResultat(): void {
-    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    this.router.navigateByUrl('/showresults/' + this.examid);
+    this.router.navigateByUrl(`/showresults/${this.examid}`);
   }
 }
 export interface ISortMobile {
