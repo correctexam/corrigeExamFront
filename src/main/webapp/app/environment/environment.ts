da40c6da2530ad10ee062eb61031cbdf8834bbeb
barbapapou <astroyelemagicien@gmail.com>
2023-01-25 11:54:19 +0100

    service default docker

--- src/main/webapp/app/environment/environment.ts
+++ src/main/webapp/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  service_url: 'http://127.0.0.1:9000',
+  service_url: 'http://127.0.0.1:443',
   cas_server_url: 'https://sso-cas6-test.univ-rennes1.fr',
 };
