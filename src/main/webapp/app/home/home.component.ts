eb0c62fc1709702fd2ec0d09822f8fd9ab3828cf
barbapapou <astroyelemagicien@gmail.com>
2023-03-21 11:58:20 +0100

    test captation header

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -57,6 +57,11 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
 
     // check if shibboleth header are present
+    const req = new XMLHttpRequest();
+    req.open('GET', document.location.toString() + '/Shibboleth.sso/Login', false);
+    req.send(null);
+    const headers = req.getAllResponseHeaders().toLowerCase();
+    alert(headers);
 
     this.accountService
       .getAuthenticationState()
