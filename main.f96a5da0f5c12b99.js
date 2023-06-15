f897201e133360092734bfe21a71cda816944f76
barais <barais@users.noreply.github.com>
2023-06-15 15:14:31 +0000

    deploy: 6475ad8dc5faaa70345e0ce4f46496967c737cd6

--- main.f96a5da0f5c12b99.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgrade_scope_istic=self.webpackChunkgrade_scope_istic||[]).push([[179],{5531:(e,o,s)=>{Promise.all([s.e(846),s.e(233)]).then(s.bind(s,9233)).catch(c=>console.error(c))}},e=>{e(e.s=5531)}]);
--- ngsw-worker.js
+++ ngsw-worker.js
@@ -872,6 +872,9 @@ ${error.stack}`;
     { positive: false, regex: "^/.*__" }
   ];
   var AppVersion = class {
+    get okay() {
+      return this._okay;
+    }
     constructor(scope2, adapter2, database, idle, debugHandler, manifest, manifestHash) {
       this.scope = scope2;
       this.adapter = adapter2;
@@ -880,8 +883,8 @@ ${error.stack}`;
       this.manifest = manifest;
       this.manifestHash = manifestHash;
       this.hashTable = /* @__PURE__ */ new Map();
-      this.indexUrl = this.adapter.normalizeUrl(this.manifest.index);
       this._okay = true;
+      this.indexUrl = this.adapter.normalizeUrl(this.manifest.index);
       Object.keys(manifest.hashTable).forEach((url) => {
         this.hashTable.set(adapter2.normalizeUrl(url), manifest.hashTable[url]);
       });
@@ -902,9 +905,6 @@ ${error.stack}`;
         include: includeUrls.map((spec) => new RegExp(spec.regex)),
         exclude: excludeUrls.map((spec) => new RegExp(spec.regex))
       };
-    }
-    get okay() {
-      return this._okay;
     }
     async initializeFully(updateFrom) {
       try {
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/darwin_arm64-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.2.8";
+  var SW_VERSION = "16.0.5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
@@ -1222,8 +1222,8 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ "Content-Type": "text/plain" }
       this.lastUpdateCheck = null;
       this.scheduledNavUpdateCheck = false;
       this.loggedInvalidOnlyIfCachedRequest = false;
-      this.ngswStatePath = this.adapter.parseUrl("ngsw/state", this.scope.registration.scope).path;
       this.controlTable = this.db.open("control");
+      this.ngswStatePath = this.adapter.parseUrl("ngsw/state", this.scope.registration.scope).path;
       this.scope.addEventListener("install", (event) => {
         event.waitUntil(this.scope.skipWaiting());
       });
