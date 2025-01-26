4a6401437aba059ca4e680fbbf636d725542a6f4
Louis.Descamps <ldescamp@insa-rennes.fr>
2025-01-26 18:28:04 +0100

    ajout de toutes les traductions automatiques manquantes plus suppression de commentaires inutiles

--- src/main/webapp/app/entities/prediction/list/list.component.ts
+++ src/main/webapp/app/entities/prediction/list/list.component.ts
@@ -5,12 +5,13 @@ import { IPrediction } from '../prediction.model';
 import { HttpResponse } from '@angular/common/http'; // Import HttpResponse
 import { NgIf, NgFor } from '@angular/common';
 import { forkJoin } from 'rxjs';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 @Component({
   selector: 'app-prediction-list',
   templateUrl: './list.component.html',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [TranslateDirective, NgFor, NgIf],
 })
 export class PredictionListComponent implements OnInit {
   predictions: IPrediction[] = []; // Array to store predictions
@@ -83,6 +84,29 @@ export class PredictionListComponent implements OnInit {
         error: err => {
           console.error(`Error deleting predictions for exam ${numericExamId}:`, err);
           alert(`Failed to delete some or all predictions for exam ${numericExamId}. Please try again.`);
+          this.loadAll();
+        },
+      });
+    }
+  }
+
+  deleteAllPredictions(): void {
+    const predictionsToDelete = this.predictions.filter(p => p.id !== undefined).map(p => this.predictionService.delete(p.id!));
+
+    if (predictionsToDelete.length === 0) {
+      alert(`No predictions found to delete`);
+      return;
+    }
+
+    if (confirm('Are you sure you want to delete all predictions?')) {
+      forkJoin(predictionsToDelete).subscribe({
+        next: () => {
+          this.loadAll();
+          alert('Successfully deleted all predictions');
+        },
+        error: err => {
+          console.error('Error deleting all predictions:', err);
+          alert('Failed to delete some or all predictions. Please try again.');
           this.loadAll();
         },
       });
