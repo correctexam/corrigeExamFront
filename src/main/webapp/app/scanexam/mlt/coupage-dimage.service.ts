5bb7263a317415854a518bc13413cafff67fa426
Olivier Barais <olivieR.barais@gmail.com>
2025-02-25 15:17:05 +0100

    remove call to python code to detect line

--- src/main/webapp/app/scanexam/mlt/coupage-dimage.service.ts
+++ src/main/webapp/app/scanexam/mlt/coupage-dimage.service.ts
@@ -8,32 +8,8 @@ import { AlignImagesService } from '../services/align-images.service';
   providedIn: 'root',
 })
 export class CoupageDimageService {
-  private apiUrl = 'http://localhost:8080/api/coupage-dimage';
-
-  constructor(
-    private http: HttpClient,
-    private alignImagesService: AlignImagesService,
-  ) {}
+  constructor(private alignImagesService: AlignImagesService) {}
   runScript(imageSrc: ImageData): Observable<any> {
-    // console.error('imageSrc:', imageSrc);
     return this.alignImagesService.getLinesFromImage({ image: imageSrc });
-  }
-
-  runScript1(imageSrc: string): Observable<any> {
-    const body = { image: imageSrc }; // Payload
-    // console.log('Sending request to:', this.apiUrl);
-    // console.log('Request body:', body);
-
-    return this.http.post(this.apiUrl, body).pipe(
-      map((response: any) => {
-        // eslint-disable-next-line no-console
-        console.log('Response:', response);
-        return response;
-      }),
-      catchError(error => {
-        console.error('Error in CoupageDimageService:', error);
-        throw error;
-      }),
-    );
   }
 }
