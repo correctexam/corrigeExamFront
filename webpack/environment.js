15c98f523d1d896fe016e96550563cf480f19dfb
Olivier Barais <olivieR.barais@gmail.com>
2025-03-10 11:30:14 +0100

    update version

--- webpack/environment.js
+++ webpack/environment.js
@@ -6,6 +6,6 @@ module.exports = {
   __CONNECTION_METHOD__: process.env.hasOwnProperty('CONNECTION_METHOD') ? process.env.CONNECTION_METHOD : 'local',
   __CAS_SERVER_URL__: process.env.hasOwnProperty('CAS_SERVER_URL') ? process.env.CAS_SERVER_URL : 'https://cas-server.com',
   __SERVICE_URL__: process.env.hasOwnProperty('SERVICE_URL') ? process.env.SERVICE_URL : 'https://correct-exam.com',
-  __VERSION__: process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : '1.5.0',
+  __VERSION__: process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : '1.6.0',
   __DEBUG_INFO_ENABLED__: false,
 };
