3bcce421135dc4d59ed6b9ced87757b02c4519f1
Olivier Barais <olivieR.barais@gmail.com>
2025-09-08 14:15:46 +0200

    update translation

--- src/main/webapp/app/account/activate/activate.component.ts
+++ src/main/webapp/app/account/activate/activate.component.ts
@@ -3,12 +3,13 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { mergeMap } from 'rxjs/operators';
 
 import { ActivateService } from './activate.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'jhi-activate',
   templateUrl: './activate.component.html',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
 })
 export class ActivateComponent implements OnInit {
   error = false;
